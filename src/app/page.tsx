9ab032c6ec8399eb93dd68141c71e1915f1e6471
DEVMorningCoffee <jeffreyabraham.sep+github@gmail.com>
2023-11-11 16:13:39 -0500

    fix not able to click appointment button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 const Header = () => {
   return (
-    <div>
-    <header className="bg-white p-8 shadow-md">
+    <div className="pb-10" id="home">
+    <header className="bg-white p-8 shadow-md sticky w-full top-0 z-50">
     <div className="container mx-auto flex justify-between items-center">
       <div className="site-title">
         <h1 className="text-blue-700 text-2xl font-light">John's Barbershop</h1>
         <p className="subtitle text-blue-500 text-sm capitalize">Getting your hair ready</p>
       </div>
       <nav className="space-x-4">
-        <ul className="flex items-center gap-10 font-bold">
-        <li><a href="index.html" className="current-page text-green-200 transition duration-300 ease-in-out">Home</a></li>
+        <ul className="flex items-center gap-10 font-bold z-50">
+        <li><a href="#home" className="current-page text-green-200 transition duration-300 ease-in-out">Home</a></li>
           <li><a href="#gallery" className="text-blue-700 hover:text-green-200 transition duration-300 ease-in-out">Gallery</a></li>
           <li><a href="#team" className="text-blue-700 hover:text-green-200 transition duration-300 ease-in-out">Team</a></li>
         </ul>
@@ -18,16 +18,18 @@ const Header = () => {
   </header>
 
   <main>
-    <div id="home" className="relative h-85vh bg-cover bg-center flex items-center">
+
+    <div className="flex items-center relative w-full h-[85vh] bg-cover bg-[url('/andre-reis-1_DAlXy0wng-unsplash.jpg')] bg-no-repeat bg-center bg-fixed">
       <div className="overlay absolute inset-0 flex items-center justify-center">
         <div className="landing-text text-white text-center opacity-100">
           <h3 className="font-bold pb-6 text-xl">Getting your hair ready</h3>
           <h1 className="text-6xl font-bold mb-6">John's Barbershop</h1>
           <hr className="w-20 h-1 bg-white mx-auto mb-6"/>
-          <a href="contact.html" className="btn-hire bg-blue-700 text-white px-8 py-4 rounded-md">Appointment</a>
+          <a href="appointment.html" className="bg-blue-700 text-white px-8 py-4 rounded-md">Appointment</a>
         </div>
       </div>
     </div>
+
     <div className="gallery-header text-center py-8" id="gallery">
       <h1 className="font-bold text-4xl text-blue-700 md:text-2xl lg:text-3xl">Some of your most awesome haircuts!</h1>
       <p className="font-semibold text-gray-800">Hope you come back soon</p>
